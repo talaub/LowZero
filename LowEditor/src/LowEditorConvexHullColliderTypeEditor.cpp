ac787293adcd902e76dbff57f4baf98f631029e0
Tamino Laub <t.laub@web.de>
2026-07-12 12:23:02 +0200

    Load convex hull collider points from mesh

--- LowEditor/src/LowEditorConvexHullColliderTypeEditor.cpp
+++ LowEditor/src/LowEditorConvexHullColliderTypeEditor.cpp
@@ -12,8 +12,14 @@
 
 #include "LowEditorThemes.h"
 #include "LowMath.h"
+#include "LowMathVectorUtil.h"
+#include "LowRendererMesh.h"
+#include "LowRendererMeshResource.h"
 #include "LowUtilContainers.h"
+#include "LowUtilHandle.h"
+#include "LowUtilLogger.h"
 #include "LowUtilString.h"
+#include "LowUtilJobManager.h"
 namespace Low {
   namespace Editor {
 
@@ -27,16 +33,110 @@ namespace Low {
           l_Entity.get_component(
               Core::Component::MeshRenderer::type_id());
 
+      Renderer::Mesh l_Mesh = Util::Handle::DEAD;
+      if (l_MeshRenderer.is_alive()) {
+        l_Mesh = l_MeshRenderer.get_mesh();
+      }
+
       if (ImGui::BeginPopupModal("Construct collider from mesh")) {
 
-        if (l_MeshRenderer.is_alive()) {
+        if (!l_MeshRenderer.is_alive()) {
+          ImGui::Text("This entity does not have a MeshRenderer "
+                      "component assigned.");
+
+          if (Gui::Button("Cancel")) {
+            ImGui::CloseCurrentPopup();
+          }
+        } else if (!l_Mesh.is_alive()) {
+          ImGui::Text("This entity's mesh renderer does not have a "
+                      "valid mesh "
+                      "assigned.");
+
+          if (Gui::Button("Cancel")) {
+            ImGui::CloseCurrentPopup();
+          }
+        } else {
           ImGui::Text(
               "The existing collider will be wiped and replaced with "
               "a version constructed from the actual mesh data.");
-        } else {
 
-          ImGui::Text("This entity does not have a MeshRenderer "
-                      "component assigned.");
+          if (Gui::Button("Cancel")) {
+            ImGui::CloseCurrentPopup();
+          }
+          ImGui::SameLine();
+          if (Gui::Button("Load")) {
+
+            Renderer::MeshResource l_MeshResource =
+                l_Mesh.get_resource();
+
+            const u64 l_ColliderId = l_Collider.get_id();
+
+            Util::JobManager::IO::schedule_read_mesh(
+                l_MeshResource.get_mesh_path(),
+                l_MeshResource.get_sidecar_path(),
+                [l_ColliderId](
+                    bool p_Success,
+                    Util::JobManager::IO::MeshLoadResult p_Result) {
+                  if (!p_Success) {
+                    LOW_LOG_ERROR << "Failed to load mesh file."
+                                  << LOW_LOG_END;
+                    return;
+                  }
+
+                  Core::Component::ConvexHullCollider l_Collider =
+                      l_ColliderId;
+
+                  l_Collider.get_points().clear();
+
+                  for (u32 i = 0; i < p_Result.mesh.submeshes.size();
+                       ++i) {
+                    const Util::Resource::Submesh &i_Submesh =
+                        p_Result.mesh.submeshes[i];
+                    for (u32 j = 0; j < i_Submesh.meshInfos.size();
+                         ++j) {
+                      const Util::Resource::MeshInfo i_MeshInfo =
+                          i_Submesh.meshInfos[j];
+
+                      for (u32 k = 0; k < i_MeshInfo.vertices.size();
+                           ++k) {
+                        const Math::Vector3 i_Point = Math::Vector3(
+                            i_Submesh.transform *
+                            Math::Vector4(
+                                i_MeshInfo.vertices[k].position,
+                                1.0f));
+
+                        bool i_Unique = true;
+
+                        for (const Math::Vector3 &i_ExPoint :
+                             l_Collider.get_points()) {
+                          const float i_Distance =
+                              Math::VectorUtil::distance_squared(
+                                  i_Point, i_ExPoint);
+                          if (i_Distance < 0.5f) {
+                            i_Unique = false;
+                            break;
+                          }
+                        }
+
+                        if (i_Unique) {
+                          l_Collider.get_points().push_back(i_Point);
+                        }
+                      }
+                    }
+                  }
+                  l_Collider.mark_dirty();
+                  LOW_LOG_INFO << "Added "
+                               << l_Collider.get_points().size()
+                               << " from mesh." << LOW_LOG_END;
+                });
+
+            Util::JobManager::Background::schedule(
+                "Loading convex collider from mesh",
+                [](Util::Function<void(float)> p_Progress) {
+                  p_Progress(0);
+                });
+            ImGui::CloseCurrentPopup();
+          }
         }
 
         ImGui::EndPopup();
